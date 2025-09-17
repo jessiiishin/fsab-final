ae30c359e0063ee403984320c333dcea9890cf29
jessiiishin <jessiiishin@gmail.com>
2025-09-16 22:14:58 -0400

    actually changed to js (reconfigured)

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,5 @@
 import Head from "next/head";
 import Image from "next/image";
-import { Geist, Geist_Mono } from "next/font/google";
-import styles from "@/styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -9,20 +7,17 @@ export default function Home() {
       <Head>
         <title>Hello World!</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" type="image/png" href="/favicon.png" />
+        <link rel="icon" href="/favicon.png" />
       </Head>
-      
       <div>
         <h1>
-        Hello World!
+          Hello World!
         </h1>
         <p>
           Are you ready to say hello?
         </p>
         <button>Let's do it</button>
       </div>
-      
-
     </>
   );
 }
